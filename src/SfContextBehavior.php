99f5f5133cc28c1842f41a6d1d5a467981005eba
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-12-05 13:49:18 +0100

    fixed bad bpackage name

--- src/SfContextBehavior.php
+++ src/SfContextBehavior.php
@@ -2,8 +2,7 @@
 /**
  * This file declare the sfContextBehavior class.
  *
- * @package Loopkey
- * @subpackage lib-propel-behavior
+ * @package propel.generator.behavior.sf_context
  * @author Julien Muetton <julien_muetton@carpe-hora.com>
  * @copyright (c) Carpe Hora SARL 2011
  * @since 2011-11-30
